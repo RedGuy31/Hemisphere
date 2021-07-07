0d1746678a8aab3e75d34c1e3ed550d774ce6787
Red guy <i.maisuradze31.19@gmail.com>
2021-07-07 04:31:34 +0400

    k

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
 constructor(props){
 super(props)
 
-this.state = { latitude:25}
+this.state = { latitude:null}
 
 window.navigator.geolocation.getCurrentPosition(
   (position) => {
