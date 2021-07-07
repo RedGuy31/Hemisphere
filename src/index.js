81274cee8d5e6dc211f46a3d296bc454c1f32606
Red guy <i.maisuradze31.19@gmail.com>
2021-07-08 01:19:47 +0400

    sr

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const App = () =>{
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (Error) => console.log(Error)
+    );
+    
     return(
-        <div>s</div>
+        <div>
+            latitude:
+        </div>
     )
 }
 
 
 ReactDOM.render(
     <App />,
-    document.querySelector('#root')
+    document.querySelector('#root') 
 )
